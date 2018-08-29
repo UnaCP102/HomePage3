1c5adbecd47b132167b1d3a5a37356bef663cb10
UnaCP102 <xu3bji4vup326@gmail.com>
2018-08-29 13:47:21 +0800

    Initial commit

---
+++ app/src/main/java/unalee/homepage3/MainActivity.java
@@ -0,0 +1,76 @@
+package unalee.homepage3;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.item_Reserved:
+                    fragment = new ReservedFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textReserved);
+                    return true;
+                case R.id.item_Date:
+                    fragment = new DateFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textDate);
+                    return true;
+                case R.id.item_Profile:
+                    fragment = new ProfileFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textProfile);
+                    return true;
+                case R.id.item_HomePage:
+                    fragment = new HomePageFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textHomePage);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        initContent();
+
+    }
+
+    private void initContent() {
+        Fragment fragment = new HomePageFragment();
+        changeFragment(fragment);
+        setTitle(R.string.textHomePage);
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/unalee/homepage3/ProfileFragment.java
@@ -0,0 +1,19 @@
+package unalee.homepage3;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class ProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_myprofile, container, false);
+        return view;
+    }
+
+}
